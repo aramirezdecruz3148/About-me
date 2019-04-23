8a7ba475fc443649b0b4a05a3627312e60aa3360
aramirezdecruz3148 <leigh3148@gmail.com>
2019-04-23 16:37:00 -0700

    created variables and assigned via document.getElementById(<id>)

---
+++ src/app.js
@@ -0,0 +1,7 @@
+const qOne = document.getElementById('q1');
+const qTwo = document.getElementById('q2');
+const qThree = document.getElementById('q3');
+const qFour = document.getElementById('q4');
+const qFive = document.getElementById('q5');
+const submitButton = document.getElementById('submit-button');
+const quizResult = document.getElementById('quiz-result');
